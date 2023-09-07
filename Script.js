f88ee869d23370ef5add93d5d6015d97acdefc9a
Chethan Kumar K V <109806203+Chethankumarkv@users.noreply.github.com>
2023-09-07 19:16:53 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,9 @@
+console.log("hi");
+
+function saveData()
+{
+    const userName = document.getElementById('userName').value;
+    const passWord = document.getElementById('password1').value;
+    const rePassword = document.getElementById('password2').value;
+    console.log(userName+passWord+rePassword);
+}
